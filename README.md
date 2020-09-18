a7ec20d5285a061e542008293efa8bf7ea050ef8
Edward Horsey <edwardhorsey@me.com>
2020-09-18 15:06:48 +0100

    initial plus readme

---
+++ README.md
@@ -0,0 +1,3 @@
+If you're like me and like to Inbox Zero, you may also like to archive your whatsapp chats..!
+
+Still WIP: 🐛 meeting occasional Stale Element Exceptions
--- archive.py
+++ archive.py
@@ -11,7 +11,8 @@ import os
 import time
 
 options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=C:\\Users\\Edward\\AppData\\Local\\Google\\Chrome\\User Data\\") 
+driver_path = f"user-data-dir=C:/Users/{os.getlogin()}/AppData/Local/Google/Chrome/User Data/"
+options.add_argument(driver_path)
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.get("https://web.whatsapp.com/")
@@ -23,17 +24,18 @@ first_chat_location = "#pane-side > div:nth-child(1) > div > div > div:nth-child
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, first_chat_location)))
 how_many_chats = driver.find_elements_by_class_name("_3ko75")
 
-def trim(arr):
-    return [item for item in arr if item]
+def format_nodes(arr):
+    trim = [item for item in arr if item]
+    return  [*trim[1:], trim[0]]
 
-chat_names = trim([chat.get_attribute('title') for chat in how_many_chats])
-print(len(how_many_chats), 'unarchived chats: ', chat_names)
+chat_names = format_nodes([chat.get_attribute('title') for chat in how_many_chats])
+print(f'{len(chat_names)} unarchived chats: , {chat_names}')
 
 def archive(user_name):
     try:
         user = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, f'//span[@title="{user_name}"]')))
         actions.context_click(user).perform()
-        time.sleep(2)
+        # time.sleep(2) ?
         list = driver.find_element_by_xpath(f'//div[@title="Archive chat"]')
         list.click()
     except StaleElementReferenceException:
@@ -46,4 +48,4 @@ for user in chat_names:
     if user:
         print(f'Archiving {user}')
         archive(user)
-        time.sleep(5)
+        # time.sleep(5) ?
